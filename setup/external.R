a043555278c1f21e6a77fde248c324e9e721f233
JuhuiJin <juhui@dhcp-10-250-4-128.harvard.edu>
2025-02-17 13:48:37 -0500

    split scripts

--- setup/external.R
+++ setup/external.R
@@ -10,3 +10,31 @@ for (pkg in packages) {
   }
   library(pkg, character.only = TRUE)
 }
+
+check_na <- function(data, var_list) {
+  # Pre-allocate the data frame with appropriate number of rows
+  na_table <- data.frame(
+    Variable = character(length = length(var_list)),
+    NAs = numeric(length = length(var_list)),
+    Not_NAs = numeric(length = length(var_list)),
+    Total_n = numeric(length = length(var_list)),
+    stringsAsFactors = FALSE
+  )
+  
+  # Loop through each variable in the var_list
+  for (i in seq_along(var_list)) {
+    var <- var_list[i]
+    na_count <- sum(is.na(data[[var]]))  # Count NA values
+    not_na_count <- sum(!is.na(data[[var]]))  # Count non-NA values
+    total_n <- nrow(data)  # Total rows in the data
+    
+    # Assign the results directly to the pre-allocated table
+    na_table$Variable[i] <- var
+    na_table$NAs[i] <- na_count
+    na_table$Not_NAs[i] <- not_na_count
+    na_table$Total_n[i] <- total_n
+  }
+  
+  return(na_table)
+}
+
