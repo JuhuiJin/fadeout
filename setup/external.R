edcae9a4394202ef2d0724085f0680e7924c33c6
JuhuiJin <juhui@dhcp-10-250-4-128.harvard.edu>
2025-02-18 00:56:11 -0500

    added figures

--- setup/external.R
+++ setup/external.R
@@ -2,7 +2,7 @@
 external_path <- "/Users/juhui/Desktop/research/data/"
 
 # Package Installments 
-packages <- c("dplyr", "tidyr", "haven", "dplyr", "ggplot2", "fixest", "sn", "moments")
+packages <- c("dplyr", "tidyr", "haven", "dplyr", "ggplot2", "fixest")
 
 for (pkg in packages) {
   if (!require(pkg, character.only = TRUE)) {
